595588d6fcfe4b7c3e470fa3609ae33d0d3d9688
Allen <40887236+AllenGong0@users.noreply.github.com>
2024-01-22 09:47:33 +0800

    Update cfw.js

--- src/cfw.js
+++ src/cfw.js
@@ -15,8 +15,6 @@ module.exports.parse = ({ content, name, url }, { yaml, axios, notify }) => {
     return !item.includes('paypal')
   }); // 干掉paypal
 
-  const common = ['DOMAIN-SUFFIX,qq.com,DIRECT', 'DOMAIN-SUFFIX,betahub.cn,DIRECT'];
-
   const paypal = ['DOMAIN-SUFFIX,paypal.com,Proxy', 'DOMAIN-SUFFIX,paypalobjects.com,Proxy'];
 
   const biying = ['DOMAIN-SUFFIX,bing.com,OpenAi']
@@ -25,13 +23,14 @@ module.exports.parse = ({ content, name, url }, { yaml, axios, notify }) => {
 
   const openAi = ['DOMAIN-SUFFIX,openai.com,OpenAi', 'DOMAIN-SUFFIX,statsigapi.net,OpenAi'];
 
-  const xrender = ['DOMAIN-SUFFIX,xrender.fun,Proxy'];
-
   const yfd = ['DOMAIN-SUFFIX,zhenguanyu.com,DIRECT', 'DOMAIN-SUFFIX,yuanfudao.biz,DIRECT'];
 
   const mlProxy = ['dropboxusercontent.com','huggingface.co'].map( item => `DOMAIN-SUFFIX,${item},Proxy`)
 
+  const steam = ['steampowered.com'].map( item => `DOMAIN-SUFFIX,${item},Proxy`)
+
   content.rules.unshift(...paypal, ...openAi, ...yfd, ...xrender, ...mlProxy);
 
+  console.log(content)
   return content;
 }
