1fe73594c3ef7b496a02fa3ed3becc82013b4792
Allen <40887236+AllenGong0@users.noreply.github.com>
2026-08-06 14:38:01 +0800

    Refactor proxy group names in clash-verge.js

--- src/clash-verge.js
+++ src/clash-verge.js
@@ -1,7 +1,16 @@
 // Clash Verge Script
 // Main function receives the config object directly
 function main(config, profilename) {
-	console.log(profilename)
+	console.log(profilename);
+	if (profilename.includes("kanyun.com.yaml")) {
+		return config;
+	}
+
+	const suyingProxyName = "速鹰666";
+	const mmyProxyName = "🔰国外流量";
+	const proxyGroupName =
+		profilename === "速鹰666" ? suyingProxyName : mmyProxyName;
+
 	const proxyGroup = config["proxy-groups"];
 
 	// Filter proxies for OpenAI group (Taiwan, US, Japan)
@@ -27,36 +36,47 @@ function main(config, profilename) {
 	});
 
 	// Custom rules
-	const github = ["DOMAIN-SUFFIX,github.com,速鹰666"];
+	const github = [`DOMAIN-SUFFIX,github.com,${proxyGroupName}`];
 
-	const cursor = ["DOMAIN-SUFFIX,cursor.com,速鹰666"];
+	const cursor = [`DOMAIN-SUFFIX,cursor.com,${proxyGroupName}`];
+
+	const google = [`DOMAIN-SUFFIX,googlesource.com,${proxyGroupName}`];
 
 	const paypal = [
-		"DOMAIN-SUFFIX,paypal.com,速鹰666",
-		"DOMAIN-SUFFIX,paypalobjects.com,速鹰666",
+		`DOMAIN-SUFFIX,paypal.com,${proxyGroupName}`,
+		`DOMAIN-SUFFIX,paypalobjects.com,${proxyGroupName}`,
 	];
 
-	const biying = ["DOMAIN-SUFFIX,bing.com,OpenAi"];
+	const biying = [`DOMAIN-SUFFIX,bing.com,OpenAi`];
 
-	const xrender = ["DOMAIN-SUFFIX,xrender.fun,速鹰666"];
+	const xrender = [`DOMAIN-SUFFIX,xrender.fun,${proxyGroupName}`];
 
 	const openAi = [
-		"DOMAIN-SUFFIX,openai.com,OpenAi",
-		"DOMAIN-SUFFIX,chatgpt.com,OpenAi",
-		"DOMAIN-SUFFIX,statsigapi.net,OpenAi",
+		`DOMAIN-SUFFIX,openai.com,OpenAi`,
+		`DOMAIN-SUFFIX,chatgpt.com,OpenAi`,
+		`DOMAIN-SUFFIX,statsigapi.net,OpenAi`,
 	];
-	const mlProxy = ["dropboxusercontent.com", "huggingface.co"].map(
-		(item) => `DOMAIN-SUFFIX,${item},速鹰666`
+
+	const yfd = [
+		`DOMAIN-SUFFIX,zhenguanyu.com,DIRECT`,
+		`DOMAIN-SUFFIX,yuanfudao.biz,DIRECT`,
+		`DOMAIN-SUFFIX,moxt.ai,DIRECT`,
+	];
+
+	const mlProxy = [`dropboxusercontent.com`, `huggingface.co`].map(
+		(item) => `DOMAIN-SUFFIX,${item},${proxyGroupName}`
 	);
 
-	const steam = ["steampowered.com"].map(
-		(item) => `DOMAIN-SUFFIX,${item},速鹰666`
+	const steam = [`steampowered.com`].map(
+		(item) => `DOMAIN-SUFFIX,${item},${proxyGroupName}`
 	);
 
-	const ns = ["nintendo.com"].map((item) => `DOMAIN-SUFFIX,${item},速鹰666`);
+	const ns = [`nintendo.com`].map(
+		(item) => `DOMAIN-SUFFIX,${item},${proxyGroupName}`
+	);
 
-	const infuse = ["firecore.com"].map(
-		(item) => `DOMAIN-SUFFIX,${item},速鹰666`
+	const infuse = [`firecore.com`].map(
+		(item) => `DOMAIN-SUFFIX,${item},${proxyGroupName}`
 	);
 
 	// Add custom rules at the beginning
@@ -64,11 +84,13 @@ function main(config, profilename) {
 		...github,
 		...paypal,
 		...openAi,
+		...yfd,
 		...xrender,
 		...mlProxy,
 		...cursor,
 		...ns,
-		...infuse
+		...infuse,
+		...google
 	);
 
 	return config;
